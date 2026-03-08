b33a47977526410617704aa44bad3085a14e319e
Viktar Zanimonets <vzanimonets@gmail.com>
2026-03-08 19:55:29 +0300

    feat: update worker thread logic

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -5,5 +5,7 @@ import { parentPort } from 'worker_threads';
 // Send back to main thread
 
 parentPort.on('message', (data) => {
-  // Write your code here
+  const numbers = Array.isArray(data) ? data : [];
+  const sorted = [...numbers].sort((a, b) => a - b);
+  parentPort.postMessage(sorted);
 });
